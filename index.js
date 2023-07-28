63b38cacbf6d810d1a0a360420c840f0c9ce53a2
TheLastVelo <jarellchinn44@gmail.com>
2023-07-27 21:15:23 -0700

    Puedocode

---
+++ index.js
@@ -0,0 +1,41 @@
+// Identify what is being asked
+    // Prompt for text and enter 3 characters
+    // Prompt for text color(color or hex number)
+    // Prompt with list of shapes
+    // Prompt for shape color (color or hex number)
+    // Create a .svg file named logo.svg
+        // Console.log("Generated logo.svg")
+    // Logo.svg should be 300x200 Px
+    // jest testing on the render 
+    // Each shape class should be tested for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
+    // Record a video demo of the app
+
+// Global variables
+    // inquirer 
+    // fileSystem fs
+    // import Classes
+
+// Questions array
+const questions = ["Enter 3 characters for the icon", 
+"What color do you want the text to be", 
+"What Shape would you like", 
+"What color would you like the shape to be",
+]
+    // 3 characters
+    // text color
+    // what shape(list)
+    // shape color
+
+// Prompt Questions 0-3
+    // generate Object
+
+// Create .svg File by sending objects to Shape.js
+// create variable logo save else if return
+    // else if statments
+        // create circle instance
+        // create triangle instance
+        // create sqaure instance 
+    // writeToFile
+        // logo variable
+
+// Test render() of each shape
